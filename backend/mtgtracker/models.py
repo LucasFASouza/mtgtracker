16933d5156c1c5fe3494762388e0c368c53d5eff
Lucas Souza <outrolucas2002@gmail.com>
2023-12-05 16:35:09 -0300

    feat: add Match to Models

--- backend/mtgtracker/models.py
+++ backend/mtgtracker/models.py
@@ -17,18 +17,6 @@ class Deck(models.Model):
 
     def __str__(self):
         return f'{self.user} - {self.deck_name}'
-    
-    def games_played_with(self):
-        return sum([game.games_played() for game in self.games.all() if self in game.your_deck.all()])
-    
-    def games_won_with(self):
-        return sum([game.games_won() for game in self.games.all() if self in game.your_deck.all()])
-    
-    def games_played_against(self):
-        return sum([game.games_played() for game in self.games.all() if self in game.opp_deck.all()])
-    
-    def games_won_against(self):
-        return sum([game.games_won() for game in self.games.all() if self in game.opp_deck.all()])
 
 class Game(models.Model):
     RESULTS_CHOICES = [
@@ -40,28 +28,31 @@ class Game(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="games")
     tags = models.ManyToManyField(Tag, blank=True)
 
-    your_deck = models.ManyToManyField(Deck, related_name="your_deck")
-    opp_deck = models.ManyToManyField(Deck, related_name="opp_deck")
+    your_deck = models.ManyToManyField(Deck, related_name="games_with")
+    opp_deck = models.ManyToManyField(Deck, related_name="games_against")
 
     notes = models.TextField(blank=True)
-
-    started_play = models.BooleanField(blank=True, null=True)
-
-    result_g1 = models.CharField(choices=RESULTS_CHOICES, max_length=1)
-    result_g2 = models.CharField(choices=RESULTS_CHOICES, max_length=1)
-    result_g3 = models.CharField(choices=RESULTS_CHOICES, max_length=1, blank=True)
-
-    mulligans_g1 = models.PositiveIntegerField(blank=True, null=True)
-    mulligans_g2 = models.PositiveIntegerField(blank=True, null=True)
-    mulligans_g3 = models.PositiveIntegerField(blank=True, null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.your_deck} vs {self.opp_deck} at {self.created_at}'
+        your_victories = self.matches.filter(result='W').count()
+        opp_victories = self.matches.filter(result='L').count()
+
+        return f'{self.your_deck.first().deck_name} {your_victories} vs {opp_victories} {self.opp_deck.first().deck_name} at {self.created_at}'
     
-    def games_played(self):
-        return 3 if self.result_g3 else 2
-    
-    def games_won(self):
-        return len([result for result in [self.result_g1, self.result_g2, self.result_g3] if result == 'W'])
+class Match(models.Model):
+    RESULTS_CHOICES = [
+        ('W', 'Win'),
+        ('L', 'Lose'),
+        ('D', 'Draw'),
+    ]
+
+    result = models.CharField(choices=RESULTS_CHOICES, max_length=1)
+    mulligans = models.PositiveIntegerField(blank=True, null=True)
+    started_play = models.BooleanField(blank=True, null=True)
+
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name="matches")
+
+    def __str__(self):
+        return f"{self.result} - with {self.mulligans} mulls on the {'play' if self.started_play else 'draw'}"
