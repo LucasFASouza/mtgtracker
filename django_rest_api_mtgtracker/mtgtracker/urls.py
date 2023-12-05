7b10ae064ed5aa8bb15f5d6f53f5becb21a7cc78
Lucas Souza <outrolucas2002@gmail.com>
2023-12-05 14:36:20 -0300

    delete everything

--- django_rest_api_mtgtracker/mtgtracker/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from mtgtracker import views
-
-# define the urls
-urlpatterns = [
-    path('games/', views.games),
-    path('games/<int:pk>/', views.game_detail),
-    path('login/', views.login),
-]
--- mtg_tracker_client/src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/login">Login</router-link>
-  </nav>
-  <h1>mtgtracker</h1>
-  <router-view/>
-</template>
