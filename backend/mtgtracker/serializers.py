b59b697ce68da7cd9e92798d8b8b116cf01db1a6
Lucas Souza <outrolucas2002@gmail.com>
2023-12-05 22:17:20 -0300

    feat: add Post and creation serializer

--- backend/mtgtracker/serializers.py
+++ backend/mtgtracker/serializers.py
@@ -19,10 +19,34 @@ class MatchSerializer(serializers.ModelSerializer):
 
 class GameSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True, read_only=True)
-    your_deck = DeckSerializer(many=True, read_only=True)
-    opp_deck = DeckSerializer(many=True, read_only=True)
-    matches = MatchSerializer(many=True, read_only=True)
+    your_deck = DeckSerializer(many=False, read_only=True)
+    opp_deck = DeckSerializer(many=False, read_only=True)
+    matches = MatchSerializer(many=False, read_only=True)
 
     class Meta:
         model = Game
         fields = ['id', 'tags', 'your_deck', 'opp_deck', 'notes', 'created_at', 'matches']
+
+    def post(self, validated_data):
+        tags_data = validated_data.pop('tags')
+        your_deck_data = validated_data.pop('your_deck')
+        opp_deck_data = validated_data.pop('opp_deck')
+        matches_data = validated_data.pop('matches')
+
+        game = Game.objects.create(**validated_data)
+
+        for tag_data in tags_data:
+            tag = Tag.objects.get_or_create(**tag_data)[0]
+            game.tags.add(tag)
+
+        your_deck = Deck.objects.get_or_create(**your_deck_data)[0]
+        game.your_deck.add(your_deck)
+
+        opp_deck = Deck.objects.get_or_create(**opp_deck_data)[0]
+        game.opp_deck.add(opp_deck)
+
+        for match_data in matches_data:
+            Match.objects.create(game=game, **match_data)
+
+        return game
+        
--- backend/mtgtracker/views.py
+++ backend/mtgtracker/views.py
@@ -10,6 +10,15 @@ class GameView(APIView):
     def get(self, request):
         games = Game.objects.all()
         serializer = GameSerializer(games, many=True)
-        
+
         return Response(serializer.data)
-                
+
+    def post(self, request):
+        serializer = GameSerializer(data=request.data)
+        print(f"\n\n\nrequest.data: {serializer}\n\n\n")
+        
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=201)
+
+        return Response(serializer.errors, status=400)
