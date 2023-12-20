b7995e858946b1763995fda6d70bcaf849590ff3
Lucas Souza <outrolucas2002@gmail.com>
2023-12-20 15:25:20 -0300

    restart mobile client

--- client-mobile/App.js
+++ client-mobile/App.js
@@ -1,38 +1,20 @@
-import "react-native-gesture-handler";
-import React, { useEffect, useState } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { LoginScreen, HomeScreen, RegistrationScreen } from "./src/screens";
-import { decode, encode } from "base-64";
-import styles from "./src/styles";
-
-if (!global.btoa) {
-  global.btoa = encode;
-}
-if (!global.atob) {
-  global.atob = decode;
-}
-
-const Stack = createStackNavigator();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [loading, setLoading] = useState(true);
-  const [token, setToken] = useState(null);
-
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {token ? (
-          <Stack.Screen name="Home">
-            {(props) => <HomeScreen {...props} extraData={token} />}
-          </Stack.Screen>
-        ) : (
-          <>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Registration" component={RegistrationScreen} />
-          </>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
