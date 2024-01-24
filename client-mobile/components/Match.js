ec5f3bcbbf37da2d9ac45fb0c153719f94243109
Lucas Souza <outrolucas2002@gmail.com>
2024-01-24 17:01:36 -0300

    test Match component with mock data

--- client-mobile/components/Match.js
+++ client-mobile/components/Match.js
@@ -3,7 +3,77 @@ import React from "react";
 import { View } from "react-native";
 import { Text, Badge, Icon } from "@rneui/themed";
 
-const Match = () => {
+MOCK_DATA = {
+  id: 7,
+  tags: [
+    {
+      id: 2,
+      tag: "Insomnia",
+      user: 1,
+    },
+  ],
+  your_deck: {
+    id: 1,
+    deck_name: "Mono U Terror",
+    user: 1,
+  },
+  opp_deck: {
+    id: 2,
+    deck_name: "Kuldotha Burn",
+    user: 1,
+  },
+  notes: "test 1",
+  created_at: "2023-12-06T19:52:26.790161Z",
+  matches: [
+    {
+      id: 4,
+      result: "W",
+      mulligans: 0,
+      started_play: false,
+      is_first_match: true,
+    },
+    {
+      id: 5,
+      result: "D",
+      mulligans: 1,
+      started_play: false,
+      is_first_match: false,
+    },
+    {
+      id: 6,
+      result: "W",
+      mulligans: 0,
+      started_play: true,
+      is_first_match: false,
+    },
+  ],
+  user: 1,
+};
+
+const Match = (matchData) => {
+  const [result, setResult] = React.useState("");
+  const [color, setColor] = React.useState("");
+  const match = matchData.match;
+
+  React.useEffect(() => {
+    let games = match.matches;
+    let results = games.map((game) => game.result);
+
+    let wins = 0;
+    let losses = 0;
+
+    results.forEach((result) => {
+      if (result === "W") {
+        wins += 1;
+      } else if (result === "L") {
+        losses += 1;
+      }
+    });
+
+    setResult(wins + "-" + losses);
+    setColor(wins > losses ? "#50FAA8" : wins < losses ? "#FA5075" : "#5F5F5F");
+  }, []);
+
   return (
     <View
       style={{
@@ -15,7 +85,7 @@ const Match = () => {
       }}
     >
       <View style={{ width: "8%" }}>
-        <Icon type="ionicon" name="ellipse" size={8} color="#50FAA8" />
+        <Icon type="ionicon" name="ellipse" size={8} color={color} />
       </View>
       <Text
         style={{
@@ -25,7 +95,7 @@ const Match = () => {
           textAlign: "center",
         }}
       >
-        Mono U Terror
+        {match.your_deck.deck_name}
       </Text>
       <Text
         style={{
@@ -36,7 +106,7 @@ const Match = () => {
           textAlign: "center",
         }}
       >
-        2-1
+        {result}
       </Text>
       <Text
         style={{
@@ -46,7 +116,7 @@ const Match = () => {
           textAlign: "center",
         }}
       >
-        Kuldotha Burn
+        {match.opp_deck.deck_name}
       </Text>
       <View style={{ width: "8%" }}>
         <Icon type="ionicon" name="chevron-down" size={18} color="#5F5F5F" />
--- client-mobile/screens/HistoryScreen.js
+++ client-mobile/screens/HistoryScreen.js
@@ -5,6 +5,317 @@ import { Text, Icon } from "@rneui/themed";
 
 import Match from "../components/Match";
 
+DATA_MOCK = [
+  {
+    id: 7,
+    tags: [
+      {
+        id: 2,
+        tag: "Insomnia",
+        user: 1,
+      },
+    ],
+    your_deck: {
+      id: 1,
+      deck_name: "Mono U Terror",
+      user: 1,
+    },
+    opp_deck: {
+      id: 2,
+      deck_name: "Kuldotha Burn",
+      user: 1,
+    },
+    notes: "test 1",
+    created_at: "2023-12-06T19:52:26.790161Z",
+    matches: [
+      {
+        id: 4,
+        result: "L",
+        mulligans: 0,
+        started_play: false,
+        is_first_match: true,
+      },
+      {
+        id: 5,
+        result: "D",
+        mulligans: 1,
+        started_play: false,
+        is_first_match: false,
+      },
+      {
+        id: 6,
+        result: "L",
+        mulligans: 0,
+        started_play: true,
+        is_first_match: false,
+      },
+    ],
+    user: 1,
+  },
+  {
+    id: 8,
+    tags: [
+      {
+        id: 2,
+        tag: "Insomnia",
+        user: 1,
+      },
+    ],
+    your_deck: {
+      id: 1,
+      deck_name: "Mono U Terror",
+      user: 1,
+    },
+    opp_deck: {
+      id: 2,
+      deck_name: "Kuldotha Burn",
+      user: 1,
+    },
+    notes: "test 2",
+    created_at: "2023-12-06T19:52:34.442057Z",
+    matches: [
+      {
+        id: 7,
+        result: "D",
+        mulligans: 0,
+        started_play: false,
+        is_first_match: true,
+      },
+      {
+        id: 8,
+        result: "D",
+        mulligans: 1,
+        started_play: false,
+        is_first_match: false,
+      },
+      {
+        id: 9,
+        result: "L",
+        mulligans: 0,
+        started_play: true,
+        is_first_match: false,
+      },
+    ],
+    user: 1,
+  },
+  {
+    id: 9,
+    tags: [
+      {
+        id: 2,
+        tag: "Insomnia",
+        user: 1,
+      },
+    ],
+    your_deck: {
+      id: 1,
+      deck_name: "Mono U Terror",
+      user: 1,
+    },
+    opp_deck: {
+      id: 3,
+      deck_name: "BG Gardens",
+      user: 1,
+    },
+    notes: "test 3",
+    created_at: "2023-12-06T19:53:03.416702Z",
+    matches: [
+      {
+        id: 10,
+        result: "W",
+        mulligans: 0,
+        started_play: false,
+        is_first_match: true,
+      },
+      {
+        id: 11,
+        result: "W",
+        mulligans: 1,
+        started_play: false,
+        is_first_match: false,
+      },
+    ],
+    user: 1,
+  },
+  {
+    id: 10,
+    tags: [
+      {
+        id: 2,
+        tag: "Insomnia",
+        user: 1,
+      },
+    ],
+    your_deck: {
+      id: 1,
+      deck_name: "Mono U Terror",
+      user: 1,
+    },
+    opp_deck: {
+      id: 4,
+      deck_name: "CAW Gates",
+      user: 1,
+    },
+    notes: "test 3",
+    created_at: "2023-12-06T19:54:23.633210Z",
+    matches: [
+      {
+        id: 12,
+        result: "L",
+        mulligans: 0,
+        started_play: false,
+        is_first_match: true,
+      },
+      {
+        id: 13,
+        result: "W",
+        mulligans: 1,
+        started_play: false,
+        is_first_match: false,
+      },
+    ],
+    user: 1,
+  },
+  {
+    id: 11,
+    tags: [
+      {
+        id: 2,
+        tag: "Insomnia",
+        user: 1,
+      },
+    ],
+    your_deck: {
+      id: 5,
+      deck_name: "Toxic Groselha",
+      user: 1,
+    },
+    opp_deck: {
+      id: 4,
+      deck_name: "CAW Gates",
+      user: 1,
+    },
+    notes: "test 3",
+    created_at: "2023-12-06T19:55:07.531554Z",
+    matches: [
+      {
+        id: 14,
+        result: "W",
+        mulligans: 0,
+        started_play: true,
+        is_first_match: true,
+      },
+      {
+        id: 15,
+        result: "L",
+        mulligans: 1,
+        started_play: false,
+        is_first_match: false,
+      },
+      {
+        id: 16,
+        result: "W",
+        mulligans: 1,
+        started_play: false,
+        is_first_match: false,
+      },
+    ],
+    user: 1,
+  },
+  {
+    id: 12,
+    tags: [
+      {
+        id: 2,
+        tag: "Insomnia",
+        user: 1,
+      },
+    ],
+    your_deck: {
+      id: 5,
+      deck_name: "Toxic Groselha",
+      user: 1,
+    },
+    opp_deck: {
+      id: 4,
+      deck_name: "CAW Gates",
+      user: 1,
+    },
+    notes: "test 3",
+    created_at: "2023-12-06T19:55:08.278600Z",
+    matches: [
+      {
+        id: 17,
+        result: "W",
+        mulligans: 0,
+        started_play: true,
+        is_first_match: true,
+      },
+      {
+        id: 18,
+        result: "L",
+        mulligans: 1,
+        started_play: false,
+        is_first_match: false,
+      },
+      {
+        id: 19,
+        result: "W",
+        mulligans: 1,
+        started_play: false,
+        is_first_match: false,
+      },
+    ],
+    user: 1,
+  },
+  {
+    id: 14,
+    tags: [
+      {
+        id: 3,
+        tag: "Insomnia PUT",
+        user: 1,
+      },
+    ],
+    your_deck: {
+      id: 8,
+      deck_name: "Hot Cats",
+      user: 1,
+    },
+    opp_deck: {
+      id: 7,
+      deck_name: "Dimir Terror",
+      user: 1,
+    },
+    notes: "test 3",
+    created_at: "2023-12-06T20:40:51.488988Z",
+    matches: [
+      {
+        id: 32,
+        result: "L",
+        mulligans: 0,
+        started_play: true,
+        is_first_match: true,
+      },
+      {
+        id: 33,
+        result: "W",
+        mulligans: 1,
+        started_play: true,
+        is_first_match: false,
+      },
+      {
+        id: 34,
+        result: "L",
+        mulligans: 1,
+        started_play: true,
+        is_first_match: false,
+      },
+    ],
+    user: 1,
+  },
+];
+
 export default function HistoryScreen() {
   return (
     <View style={{ backgroundColor: "#282828", height: "100%" }}>
@@ -44,10 +355,9 @@ export default function HistoryScreen() {
           borderTopWidth: 1,
         }}
       >
-        <Match />
-        <Match />
-        <Match />
-        <Match />
+        {DATA_MOCK.map((match) => {
+          return <Match key={match.id} match={match} />;
+        })}
       </View>
     </View>
   );
